0498ef71feb7728017503948c21949e40e290e04
mehmetefeoglu <mehmet.efeogludev@gmail.com>
2025-04-16 13:51:41 +0300

    görevler yapıldı

--- src/main/java/com/workintech/sqlintro/entity/Ogrenci.java
+++ src/main/java/com/workintech/sqlintro/entity/Ogrenci.java
@@ -21,4 +21,60 @@ public class Ogrenci {
     private int puan;
     @Column(nullable = true)
     private String dtarih;
+
+    public Long getOgrno() {
+        return ogrno;
+    }
+
+    public void setOgrno(Long ogrno) {
+        this.ogrno = ogrno;
+    }
+
+    public String getAd() {
+        return ad;
+    }
+
+    public void setAd(String ad) {
+        this.ad = ad;
+    }
+
+    public String getSoyad() {
+        return soyad;
+    }
+
+    public void setSoyad(String soyad) {
+        this.soyad = soyad;
+    }
+
+    public String getCinsiyet() {
+        return cinsiyet;
+    }
+
+    public void setCinsiyet(String cinsiyet) {
+        this.cinsiyet = cinsiyet;
+    }
+
+    public String getSinif() {
+        return sinif;
+    }
+
+    public void setSinif(String sinif) {
+        this.sinif = sinif;
+    }
+
+    public int getPuan() {
+        return puan;
+    }
+
+    public void setPuan(int puan) {
+        this.puan = puan;
+    }
+
+    public String getDtarih() {
+        return dtarih;
+    }
+
+    public void setDtarih(String dtarih) {
+        this.dtarih = dtarih;
+    }
 }
